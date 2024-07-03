7a1fa3e7c534115aa7b4662ce24022a46bb699ca
Emil Osmanbegović <hello@emilosman.com>
2024-07-03 16:56:26 +0200

    generate articles script

---
+++ generate_index.py
@@ -0,0 +1,32 @@
+#!python3
+
+import os
+
+def get_title(file_path):
+    with open(file_path, "r") as f:
+        for line in f:
+            title = line.strip()
+            if title:
+                return title.replace("# ", "")
+    return "Untitled"
+
+def generate_index():
+    index_path = "docs/articles.md"
+    with open(index_path, "w") as f:
+        f.write("# Articles\n")
+        f.write("Ripped straight from my notebooks, so excuse the shorthands and incompleteness.\n\n")
+        file_list = []
+        for root, dirs, files in os.walk("docs/pages"):
+            for file in files:
+                if file.endswith(".md") and file != "index.md":
+                    path = os.path.join(root, file)
+                    rel_path = os.path.relpath(path, "docs")
+                    file_list.append(rel_path)
+        file_list.sort()
+        for rel_path in file_list:
+            url = "/" + rel_path.replace("\\", "/").replace(".md", "")
+            title = get_title(os.path.join("docs", rel_path))
+            f.write(f"- [{title}]({url})\n")
+
+if __name__ == "__main__":
+    generate_index()
